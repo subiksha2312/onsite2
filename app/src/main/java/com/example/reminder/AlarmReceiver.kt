09bd55a262829f9df277fa78651a6c771741fd56
subiksha2312 <subikshasuresh12@gmail.com>
2021-08-26 21:59:53 +0530

    final version

--- app/src/main/java/com/example/reminder/AlarmReceiver.kt
+++ app/src/main/java/com/example/reminder/AlarmReceiver.kt
@@ -1,32 +1,32 @@
 package com.example.reminder
 
 import android.R
-import android.app.Notification
+import android.app.*
 import android.content.BroadcastReceiver
 import androidx.core.app.NotificationCompat
 
-import android.app.NotificationChannel
-
-import android.app.NotificationManager
-
 import android.os.Build
 
-import android.app.PendingIntent
 import android.content.Context
 
 import android.content.Intent
 import android.media.RingtoneManager
 import android.net.Uri
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 
 class AlarmReceiver : BroadcastReceiver() {
-    private val CHANNEL_ID = "CHANNEL_SAMPLE"
+
+    companion object {
+        private val CHANNEL_ID = "CHANNEL_SAMPLE"
+        private var bAlarmDismissed = false
+    }
 
     override fun onReceive(context: Context, intent: Intent) {
 
-        fun playtouchingsquares() { //audio for touching square
-            soundPoolAudio.play(audio ,1f, 1f, 4, 0, 2f)
-        }
+        val intentAction = intent.action
+        Log.d("Receiver1", "$intentAction")
 
         // Get id & message
         val notificationId = intent.getIntExtra("notification", 0)
@@ -36,31 +36,42 @@ class AlarmReceiver : BroadcastReceiver() {
         val mainIntent = Intent(context, MainActivity::class.java)
         val contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0)
 
-        // NotificationManager
-        val notificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // For API 26 and above
-            val channelName: CharSequence = "My Notification"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, channelName, importance)
-            notificationManager.createNotificationChannel(channel)
+        if (intentAction == "com.android.deskclock.ALARM_DONE") {
+
+            val alarmManager = context.getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager
+            alarmManager.cancel(contentIntent)
+            bAlarmDismissed = true
         }
 
-        // Prepare Notification
-        val alarmSound: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_dialog_info)
-            .setContentText(message)
-            .setContentIntent(contentIntent)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-            .setSound(alarmSound)
+        else if (!bAlarmDismissed) {
 
-        var notification: Notification = builder.build()
-        notification.flags = notification.flags or Notification.FLAG_INSISTENT
+            // NotificationManager
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        notificationManager.notify(notificationId, notification)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // For API 26 and above
+                val channelName: CharSequence = "My Notification"
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(CHANNEL_ID, channelName, importance)
+                notificationManager.createNotificationChannel(channel)
+            }
+
+            // Prepare Notification
+            val alarmSound: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+            val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_dialog_info)
+                .setContentText(message)
+                .setContentIntent(contentIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setSound(alarmSound)
+
+            var notification: Notification = builder.build()
+            notification.flags = notification.flags or Notification.FLAG_INSISTENT
+
+            notificationManager.notify(notificationId, notification)
+        }
 
     }
 }
--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.reminder
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.SoundPool
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.text.format.Time
 import android.util.Log
 import android.view.View
@@ -17,40 +21,75 @@ import android.widget.EditText
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.util.*
 
-var audio= 0
-lateinit var soundPoolAudio : SoundPool
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
     val notif: Int =1
+    private var bAlarmAccess : Boolean = false
+    private var _ALARMSETREQUEST = 100
+    private lateinit var mReceiver : AlarmReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkAlarmPermission()
+
         var cancelBtn = findViewById<Button>(R.id.cancelBtn)
         var setBtn= findViewById<Button>(R.id.setBtn)
 
         setBtn.setOnClickListener(this)
         cancelBtn.setOnClickListener(this)
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            val audioAttrib = AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .build()
-            val builder = SoundPool.Builder()
-            builder.setAudioAttributes(audioAttrib).setMaxStreams(6)  //for audio effects
-
-            soundPoolAudio = builder.build()
-
-        } else {
-            soundPoolAudio = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        }
-
-        audio= soundPoolAudio.load(this, R.raw.idk, 1)
+        mReceiver = AlarmReceiver()
+        var filter : IntentFilter = IntentFilter()
+        filter.addAction("com.android.deskclock.ALARM_DISMISS")
+        filter.addAction("com.android.deskclock.ALARM_DONE")
+        registerReceiver(mReceiver, filter)
 
     }
+
+    private fun checkAlarmPermission() {
+        val alarmPermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.SET_ALARM)
+        if (alarmPermission != PackageManager.PERMISSION_GRANTED) {
+            Log.d("permission1.1", "Making alarm set request")
+            makeAlarmPermRequest()
+        }
+        else {
+            Log.d("permission1.2", "alarm Permission already granted")
+            bAlarmAccess = true
+        }
+
+    }
+
+    private fun makeAlarmPermRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.SET_ALARM),
+            _ALARMSETREQUEST)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            _ALARMSETREQUEST -> {
+                Log.d("permission1.3", "Receiving alarm permission")
+                bAlarmAccess = !(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                Log.d("permission1.4", "perm granted = $bAlarmAccess")
+
+            }
+        }
+    }
+
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onClick(v: View) {
@@ -70,6 +109,8 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
         when (v.id) {
 
              R.id.setBtn ->   {
+
+
                  Log.d("enter1","entering1")
                 val hour = timePicker.currentHour
                 val minute = timePicker.currentMinute
@@ -88,21 +129,31 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
                  val priorAlarm: Long = priortime.getTimeInMillis()
                  Log.d("alarms","${alarmStartTime},${priorAlarm}")
 
+                 val clockIntent = Intent(AlarmClock.ACTION_SET_ALARM)
+                 clockIntent.putExtra(AlarmClock.EXTRA_HOUR, hour)
+                 clockIntent.putExtra(AlarmClock.EXTRA_MINUTES, (minute-2))
+                 clockIntent.putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+                 startActivity(clockIntent)
+
+                 /*
+
                       alarmManager.setExactAndAllowWhileIdle(
                           AlarmManager.RTC_WAKEUP,
                           priorAlarm,
                           pendingIntent
                       )
 
-                 alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmStartTime, pendingIntent)
+                  */
+
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmStartTime, pendingIntent)
                 Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show()
             }
-             R.id.cancelBtn -> {
+
+            R.id.setBtn -> {
                 alarmManager.cancel(pendingIntent)
-                Toast.makeText(this, "Canceled.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+
             }
         }
-
-
     }
 }
